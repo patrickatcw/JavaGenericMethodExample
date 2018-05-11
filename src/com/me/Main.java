3656515298836395a0e166430478ac82a22b8503
patrickatcw <pj8392@yahoo.com>
2018-05-10 21:28:56 -0400

    initial structure, and updated gitignore

---
+++ src/com/me/Main.java
@@ -0,0 +1,17 @@
+package com.me;
+/*can also write generic functions that can be called with
+different types of arguments based on the type of arguments
+passed to generic method, the compiler handles each method
+
+https://www.geeksforgeeks.org/generics-in-java/
+
+https://docs.oracle.com/javase/tutorial/java/generics/why.html
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
